05097c319ba3885ae480072608fceb6f08e86981
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-11-13 16:07:22 +0100

    sv

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let immagineCorrente = 0;
 const immagini = document.querySelectorAll("#container img");
-immagini[immagineCorrente].classList.add("active")
+/**immagini[immagineCorrente].classList.add("active")**/
 
 
 
@@ -18,6 +18,12 @@ const slide = [
 
 slide.forEach((element,index,array )=> {
     
+    let container = document.getElementById("imgContainer")
+
+    container.innerHTML += element.immagine
+    
     console.log(element.immagine);
+    console.log(element.titolo);
+    console.log(element.sottotitolo);
 
 });
