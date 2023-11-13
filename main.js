c3494fe183b08531727532ff7f898fd6a6a0d0b7
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-11-13 16:50:56 +0100

    sv

--- main.js
+++ main.js
@@ -9,8 +9,16 @@ const slide = [
         immagine: "01.webp",
         titolo: "spiderman",
         sottotitolo: "questoespiderman",
-
-
+    },
+    {
+        immagine: "02.webp",
+        titolo: "batman",
+        sottotitolo: "questoesBatman",
+    },
+    {
+        immagine: "03.webp",
+        titolo: "winx",
+        sottotitolo: "questoewinx",
     },
 
 
@@ -18,10 +26,20 @@ const slide = [
 
 slide.forEach((element,index,array )=> {
     
-    let container = document.getElementById("imgContainer")
+    let container = document.getElementById("Container")
+    let titolo =  document.getElementById("titolo")
+    let sottotitolo = document.getElementById("sottotitolo")
+    container.innerHTML = "<img src="+element.immagine+" width='600' height='400'/>"
+    titolo.innerHTML = "<p>"+element.titolo+"</p>"
+    sottotitolo.innerHTML = element.sottotitolo
+   
+   document.getElementById("avanti").addEventListener("click",function(){
 
-    container.innerHTML = "<img src="+element.immagine+" width='100' height='25'/>"
-    
+        slide.index++;
+   })
+
+
+
     console.log(element.immagine);
     console.log(element.titolo);
     console.log(element.sottotitolo);
